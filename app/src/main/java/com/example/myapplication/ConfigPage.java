fadf649866d964435073a01bcf7623f7dac3d0fc
anastasiamasalova2020 <masnas2004@gmail.com>
2023-02-11 17:34:07 -0500

    Designed a welcoming page. Created configuration page to choose different properties of a player.

---
+++ app/src/main/java/com/example/myapplication/ConfigPage.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class ConfigPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+    public void startGame(View v){
+        startActivity(new Intent(ConfigPage.this, GamePage.class));
+    }
+
+}
