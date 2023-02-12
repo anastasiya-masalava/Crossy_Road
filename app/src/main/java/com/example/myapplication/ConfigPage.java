038ffb04b8bb972affc3b9ed50dc7bc4858aa271
Alena Tochilkina <alenatochilkina11@gmail.com>
2023-02-11 19:06:07 -0500

    retrieved name from TextInputEditText

--- app/src/main/java/com/example/myapplication/ConfigPage.java
+++ app/src/main/java/com/example/myapplication/ConfigPage.java
@@ -2,9 +2,12 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ConfigPage extends AppCompatActivity {
 
@@ -14,8 +17,12 @@ public class ConfigPage extends AppCompatActivity {
         setContentView(R.layout.activity_config);
     }
     public void startGame(View v){
-        String textInput = findViewById(R.id.entered_name).toString();
-
+        TextInputEditText textInputElement = findViewById(R.id.entered_name);
+        String enteredName = textInputElement.getText().toString();
+        System.out.println(enteredName);
+        if(enteredName == null){
+            //Write error message
+        }
         startActivity(new Intent(ConfigPage.this, GamePage.class));
     }
 
